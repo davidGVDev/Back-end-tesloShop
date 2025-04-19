d9abaf8e570303843e7a7b67a1575252ff44868d
David <david38ing@gmail.com>
2025-04-19 14:34:29 -0600

    Typeorm and Postgresss connection

---
+++ src/products/entities/product.entity.ts
@@ -0,0 +1,14 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity()
+export class Product {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column('text',{
+        unique: true,
+    })
+    title: string;
+    
+    
+}
